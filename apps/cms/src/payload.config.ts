a8c658aba17983b44982b8053cddcf18764caa5f
Ji Park <ji.park@jpnws.com>
2024-03-22 14:37:48 -0700

    Refactor payload.config.ts to import only necessary features in lexicalEditor

--- apps/cms/src/payload.config.ts
+++ apps/cms/src/payload.config.ts
@@ -4,12 +4,7 @@ import { payloadCloud } from "@payloadcms/plugin-cloud";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { buildConfig } from "payload/config";
-import {
-  BlocksFeature,
-  LinkFeature,
-  lexicalEditor,
-  BlockNode,
-} from "@payloadcms/richtext-lexical";
+import { BlocksFeature, lexicalEditor } from "@payloadcms/richtext-lexical";
 
 import Users from "./collections/Users";
 import Projects from "./collections/Projects";
@@ -19,23 +14,6 @@ import Categories from "./collections/Categories";
 const editor = lexicalEditor({
   features: ({ defaultFeatures }) => [
     ...defaultFeatures,
-    LinkFeature({
-      fields: [
-        {
-          name: "rel",
-          label: "Rel Attribute",
-          type: "select",
-          hasMany: true,
-          options: ["noopener", "noreferrer", "nofollow"],
-          admin: {
-            description:
-              "The rel attribute defines the relationship between a linked resource and the current document. This is a custom link field.",
-          },
-        },
-      ],
-    }),
-    // This is incredibly powerful. You can re-use your Payload blocks
-    // directly in the Lexical editor as follows:
     BlocksFeature({
       blocks: [CodeBlock],
     }),
