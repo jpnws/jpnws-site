7bf001ca62e970a1319ceeb04a708f24370a7404
Ji Park <ji.park@jpnws.com>
2024-03-22 12:59:10 -0700

    Add Categories collection configuration

---
+++ apps/cms/src/collections/Categories.ts
@@ -0,0 +1,19 @@
+import { CollectionConfig } from "payload/types";
+
+const Category: CollectionConfig = {
+  slug: "categories",
+  admin: {
+    useAsTitle: "name",
+  },
+  fields: [
+    {
+      name: "name",
+      label: "Category Name",
+      type: "text",
+      required: true,
+      unique: true,
+    },
+  ],
+};
+
+export default Category;
