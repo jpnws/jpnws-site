10821b4204b19863f218b7ae144260787a6466b9
Ji Park <ji.park@jpnws.com>
2024-03-25 22:15:00 -0700

    Remove the sizes.

--- apps/cms/src/blocks/MediaBlock.ts
+++ apps/cms/src/blocks/MediaBlock.ts
@@ -4,21 +4,6 @@ export const MediaBlock: Block = {
   slug: "media",
   fields: [
     {
-      name: "size",
-      defaultValue: "default",
-      options: [
-        {
-          label: "Thumbnail",
-          value: "thumbnail",
-        },
-        {
-          label: "Large",
-          value: "large",
-        },
-      ],
-      type: "select",
-    },
-    {
       name: "media",
       relationTo: "media",
       required: true,
