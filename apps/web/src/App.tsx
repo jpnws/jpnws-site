5e596f3acaa393ff32e8ec0ccf5b8fd15e232cda
Ji Park <ji.park@jpnws.com>
2024-03-28 14:39:31 -0700

    Add API data fetching in App component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+
 const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/globals/header?locale=undefined&draft=false&depth=1`,
+      );
+      const data = await response.json();
+      console.log(data);
+    };
+    fetchData();
+  });
+
   return (
     <div>
       <h1>Hello World</h1>
