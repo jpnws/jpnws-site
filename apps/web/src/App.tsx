6467c3ae54c989c8e5b44a57a7063045be6a27fe
Ji Park <ji.park@jpnws.com>
2024-03-29 17:20:29 -0700

    Add root container to App component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,12 @@
 import Header from "./Header/Header";
+import styles from "./App.module.css";
 
 const App = () => {
-  return <Header />;
+  return (
+    <div className={styles.rootContainer}>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
