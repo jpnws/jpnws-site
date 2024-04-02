4eb65730bdaee61ef45f56ed20024023df70cebb
Ji Park <ji.park@jpnws.com>
2024-04-01 20:44:09 -0700

    Add date-fns dependency and update FeaturedArticle component

--- apps/web/src/Home/FeaturedArticle.tsx
+++ apps/web/src/Home/FeaturedArticle.tsx
@@ -1,5 +1,31 @@
 import styles from "./FeaturedArticle.module.css";
+import { parseISO, format } from "date-fns";
 
-const FeaturedArticle = ({ article }: { article: any }) => {};
+const FeaturedArticle = ({ article }: { article: any }) => {
+  const categories = article.categories.map((category: any) => {
+    return (
+      category.name !== "Featured" && (
+        <span key={category.id} className={styles.category}>
+          {category.name}
+        </span>
+      )
+    );
+  });
+
+  return (
+    <div className={styles.featuredArticle}>
+      <a className={styles.articleLink} href={`/articles/${article.slug}`}>
+        <h2 className={styles.title}>{article.title}</h2>
+        <div className={styles.publishedDate}>
+          {format(parseISO(article.publishedDate), "MMMM d, yyyy")}
+        </div>
+        <p className={styles.shortDescription}>
+          {article.hero.shortDescription}
+        </p>
+        <span className={styles.categories}>{categories}</span>
+      </a>
+    </div>
+  );
+};
 
 export default FeaturedArticle;
--- apps/web/src/Home/FeaturedArticles.tsx
+++ apps/web/src/Home/FeaturedArticles.tsx
@@ -1,5 +1,31 @@
+import FeaturedArticle from "./FeaturedArticle";
 import styles from "./FeaturedArticles.module.css";
+import { useEffect, useState } from "react";
 
-const FeaturedArticles = () => {};
+const FeaturedArticles = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/articles?where[featured][equals]=true`,
+      );
+      const { docs } = await response.json();
+      docs.sort((a: any, b: any) => a.featuredPriority - b.featuredPriority);
+      console.log(docs);
+      setArticles(docs);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className={styles.featuredArticles}>
+      {articles.map((article: any) => {
+        return <FeaturedArticle key={article.id} article={article} />;
+      })}
+    </div>
+  );
+};
 
 export default FeaturedArticles;
