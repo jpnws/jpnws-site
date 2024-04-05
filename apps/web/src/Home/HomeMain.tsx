1a683075a83420ee60c5131364734e4defc76f46
Ji Park <ji.park@jpnws.com>
2024-04-04 21:12:58 -0700

    Update CSS styles for featured articles, projects, and home page

--- apps/web/src/Home/HomeMain.tsx
+++ apps/web/src/Home/HomeMain.tsx
@@ -4,11 +4,13 @@ import styles from "./HomeMain.module.css";
 
 const HomeMain = () => {
   return (
-    <div className={styles.homeMain}>
-      <h2 className={styles.projectSectionTitle}>Projects</h2>
-      <FeaturedProjects />
-      <h2 className={styles.projectSectionTitle}>Articles</h2>
-      <FeaturedArticles />
+    <div className={styles.homeOuterContainer}>
+      <div className={styles.homeMain}>
+        <h2 className={styles.projectSectionTitle}>Projects</h2>
+        <FeaturedProjects />
+        <h2 className={styles.projectSectionTitle}>Articles</h2>
+        <FeaturedArticles />
+      </div>
     </div>
   );
 };
