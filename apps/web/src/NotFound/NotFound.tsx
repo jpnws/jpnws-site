c33cd14305a98b0f97149beba8176323da52d9ed
Ji Park <ji.park@jpnws.com>
2024-04-08 19:30:33 -0700

    Add NotFound module and component

---
+++ apps/web/src/NotFound/NotFound.tsx
@@ -0,0 +1,13 @@
+import styles from "./NotFound.module.css";
+
+const NotFound = () => {
+  return (
+    <div className={styles.heroContainer}>
+      <div className={styles.innerContainer}>
+        <h1>404 Not Found</h1>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
