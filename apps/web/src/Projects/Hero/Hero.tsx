e5075d8641c72f6643550b4274c4a68a6b5fe711
Ji Park <ji.park@jpnws.com>
2024-04-08 20:29:42 -0700

    Refactor Hero module CSS and update Hero component to fetch and display page title

--- apps/web/src/Projects/Hero/Hero.tsx
+++ apps/web/src/Projects/Hero/Hero.tsx
@@ -1,17 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // import RichText from "../../components/RichText";
 import styles from "./Hero.module.css";
 import Breadcrumb from "../../components/Breadcrumb";
 // import { Media } from "../../../../cms/src/payload-types";
 
 const Hero = () => {
+  const [title, setTitle] = useState<string | null>(null);
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
         `${import.meta.env.VITE_API_URL}/api/pages/6607538b5c32a56efd1cffbb?locale=undefined&draft=true&depth=1`,
       );
-      const data = await response.json();
-      console.log(data);
+      const { title } = await response.json();
+      setTitle(title);
     };
 
     fetchData();
@@ -21,6 +23,7 @@ const Hero = () => {
     <div className={styles.heroContainer}>
       <div className={styles.innerContainer}>
         <Breadcrumb />
+        <h1>{title}</h1>
       </div>
     </div>
   );
