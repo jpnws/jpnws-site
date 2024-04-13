a412ecd7841bebc67964a02e3169739c1545f9c8
Ji Park <ji.park@jpnws.com>
2024-04-12 20:59:34 -0700

    Add react-ui-scrollspy package and update dependencies in package.json files

--- apps/web/src/ProjectInfoPage/OnThisPage.tsx
+++ apps/web/src/ProjectInfoPage/OnThisPage.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import ScrollSpy from "react-scrollspy-navigation";
-
 import { formatSlug } from "../utils";
 
 import styles from "./OnThisPage.module.css";
+
+import ScrollSpy from "react-scrollspy-navigation";
 
 interface Section {
   tag: string;
@@ -23,14 +23,18 @@ const OnThisPage: React.FC<OnThisPageProps> = ({ content }) => {
   const h2s = content?.root.children.filter((item) => item.tag === "h2") || [];
 
   return (
-    <ScrollSpy activeClass={styles.navActive} offsetTop={80}>
+    <ScrollSpy offsetTop={80} activeAttr={true}>
       <nav className={styles.nav}>
         <ul className={styles.list}>
           {h2s.map((item, idx) => {
             const slug = formatSlug(item.children[0].text);
             return (
               <li className={styles.item} key={idx}>
-                <a href={`#${slug}`} className={styles.link}>
+                <a
+                  href={`#${slug}`}
+                  scrollspy-id={slug}
+                  className={styles.link}
+                >
                   {item.children[0].text}
                 </a>
               </li>
