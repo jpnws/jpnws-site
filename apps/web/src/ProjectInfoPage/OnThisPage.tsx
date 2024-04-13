7457db581a43e1cbd5eadf0929cec62c57778dd9
Ji Park <ji.park@jpnws.com>
2024-04-12 20:26:28 -0700

    Update dependencies in package.json files and refactor OnThisPage component

--- apps/web/src/ProjectInfoPage/OnThisPage.tsx
+++ apps/web/src/ProjectInfoPage/OnThisPage.tsx
@@ -1,6 +1,9 @@
-import React, { useRef, useMemo } from "react";
-import { Scrollspy } from "@makotot/ghostui";
+import React from "react";
+
+import ScrollSpy from "react-scrollspy-navigation";
+
 import { formatSlug } from "../utils";
+
 import styles from "./OnThisPage.module.css";
 
 interface Section {
@@ -19,35 +22,23 @@ interface OnThisPageProps {
 const OnThisPage: React.FC<OnThisPageProps> = ({ content }) => {
   const h2s = content?.root.children.filter((item) => item.tag === "h2") || [];
 
-  const sectionRefs = useMemo(
-    () => h2s.map(() => React.createRef<HTMLDivElement>()),
-    [h2s],
-  );
-
   return (
-    <nav className={styles.nav}>
-      <Scrollspy sectionRefs={sectionRefs}>
-        {({ currentElementIndexInViewport }) => (
-          <ul className={styles.list}>
-            {h2s.map((item, idx) => {
-              const slug = formatSlug(item.children[0].text);
-              return (
-                <li
-                  key={idx}
-                  className={
-                    currentElementIndexInViewport === idx ? styles.active : ""
-                  }
-                >
-                  <a href={`#${slug}`} className={styles.link}>
-                    {item.children[0].text}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        )}
-      </Scrollspy>
-    </nav>
+    <ScrollSpy activeClass={styles.navActive} offsetTop={80}>
+      <nav className={styles.nav}>
+        <ul className={styles.list}>
+          {h2s.map((item, idx) => {
+            const slug = formatSlug(item.children[0].text);
+            return (
+              <li className={styles.item} key={idx}>
+                <a href={`#${slug}`} className={styles.link}>
+                  {item.children[0].text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </ScrollSpy>
   );
 };
 
