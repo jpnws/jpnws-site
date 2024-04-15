f5a272e952708118f9d356c6ba4e0d6ac8177586
Ji Park <ji.park@jpnws.com>
2024-04-14 22:54:08 -0700

    Update CSS styles in multiple components

--- apps/web/src/Projects/ProjectsPage.tsx
+++ apps/web/src/Projects/ProjectsPage.tsx
@@ -1,14 +1,20 @@
+import { useContext } from "react";
 import FeaturedProjects from "./FeaturedProjects";
 import Hero from "./Hero";
 import OtherProjects from "./OtherProjects";
 
 import styles from "./ProjectsPage.module.css";
+import { ThemeContext } from "../ThemeContext";
 
 const ProjectsPage = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <>
       <Hero />
-      <main className={styles.container}>
+      <main
+        className={`${styles.container} ${theme === "dark" ? styles.containerDark : styles.containerLight}`}
+      >
         <div className={styles.main}>
           <FeaturedProjects />
           <OtherProjects />
--- apps/web/src/components/ExternalLinkIcon/ExternalLinkIcon.tsx
+++ apps/web/src/components/ExternalLinkIcon/ExternalLinkIcon.tsx
@@ -1,20 +1,22 @@
+import { useContext } from "react";
 import styles from "./ExternalLinkIcon.module.css";
+import { ThemeContext } from "../../ThemeContext";
 
 const ExternalLinkIcon = () => {
+  const { theme } = useContext(ThemeContext);
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="13"
       height="14"
-      fill="none"
       viewBox="0 0 13 14"
     >
       <path
-        className={styles.fillColor}
+        className={`${theme === "dark" ? styles.fillColorDark : styles.fillColorLight}`}
         d="M.688.875h3.937v.875h-3.5v10.5h10.5v-3.5h.875v3.938l-.437.437H.688l-.438-.438V1.313L.688.875z"
       ></path>
       <path
-        className={styles.fillColor}
+        className={`${theme === "dark" ? styles.fillColorDark : styles.fillColorLight}`}
         d="M12.5 1.313V7h-.875V2.369L5.712 8.28l-.618-.618 5.912-5.913h-4.63V.875h5.687l.437.438z"
       ></path>
     </svg>
