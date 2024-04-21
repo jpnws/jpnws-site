839714f0556a6a2bc2f9d399c5fabbd2fbf62a8a
Ji Park <ji.park@jpnws.com>
2024-04-21 12:39:42 -0700

    Update CSS styles in multiple components

--- web/src/ProjectPage/OnThisPage.tsx
+++ web/src/ProjectPage/OnThisPage.tsx
@@ -4,8 +4,6 @@ import { formatSlug } from "../utils";
 
 import styles from "./OnThisPage.module.css";
 
-import ScrollSpy from "react-scrollspy-navigation";
-
 interface Section {
   tag: string;
   children: Array<{ text: string }>;
@@ -23,26 +21,20 @@ const OnThisPage: React.FC<OnThisPageProps> = ({ content }) => {
   const h2s = content?.root.children.filter((item) => item.tag === "h2") || [];
 
   return (
-    <ScrollSpy activeClass={styles.navActive} offsetTop={80} activeAttr={true}>
-      <nav className={styles.nav}>
-        <ul className={styles.list}>
-          {h2s.map((item, idx) => {
-            const slug = formatSlug(item.children[0].text);
-            return (
-              <li className={styles.item} key={idx}>
-                <a
-                  href={`#${slug}`}
-                  scrollspy-id={slug}
-                  className={styles.link}
-                >
-                  {item.children[0].text}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    </ScrollSpy>
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
+        {h2s.map((item, idx) => {
+          const slug = formatSlug(item.children[0].text);
+          return (
+            <li className={styles.item} key={idx}>
+              <a href={`#${slug}`} className={styles.link}>
+                {item.children[0].text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
 };
 
