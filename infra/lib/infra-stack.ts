59398cb5b54dc27db6dd4fba3d27b52743752931
Ji Park <ji.park@jpnws.com>
2024-05-06 00:18:01 -0700

    refactor: Update infra code for DocumentDB, ACM, and Route53

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,109 +1,245 @@
-import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import * as cdk from "aws-cdk-lib";
+import * as cmg from "aws-cdk-lib/aws-certificatemanager";
+import * as ddb from "aws-cdk-lib/aws-docdb";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
-import {
-  InstanceClass,
-  InstanceSize,
-  InstanceType,
-  SubnetType,
-  Vpc,
-} from "aws-cdk-lib/aws-ec2";
-import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as elb from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as r53 from "aws-cdk-lib/aws-route53";
+import * as rtg from "aws-cdk-lib/aws-route53-targets";
+import * as smg from "aws-cdk-lib/aws-secretsmanager";
 import { Construct } from "constructs";
-import { ACM } from "./ACM";
-import { DocumentDB } from "./DocumentDB";
-import { ECS } from "./ECS";
-import { Route53 } from "./Route53";
-import { S3 } from "./S3";
+import { resolve } from "path";
+import {
+  backend_subdomain as backendSubdomain,
+  domain_name as domainName,
+} from "../../config.json";
 
-export class InfraStack extends Stack {
-  public readonly acm: ACM;
-  public readonly ecs: ECS;
-  public readonly docDb: DocumentDB;
-  public readonly route53: Route53;
-  public readonly s3: S3;
-  public readonly vpc: Vpc;
-
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class InfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Initialize the Route53 component
-    this.route53 = new Route53(this, "Route53");
+    // * =====================================
+    // * Virtual Private Cloud (VPC)
+    // * =====================================
 
-    // Set up ACM with the hosted zone
-    this.acm = new ACM(this, "ACM", {
-      hostedZone: this.route53.hostedZone,
-    });
-
-    // Create a VPC with public, private (egress), and isolated subnets
-    this.vpc = new Vpc(this, "MyVPC", {
-      maxAzs: 2, // Adjust as per your requirements
+    // Define the VPC and Security Group. You must define the public subnet
+    // where containerized apps are deployed and the private subnet where
+    // DocumentDB is deployed.
+    const vpc = new ec2.Vpc(this, "VPC", {
       subnetConfiguration: [
         {
           cidrMask: 24,
-          name: "ingress",
-          subnetType: SubnetType.PUBLIC,
+          name: "public",
+          subnetType: ec2.SubnetType.PUBLIC,
         },
         {
           cidrMask: 24,
-          name: "compute",
-          subnetType: SubnetType.PRIVATE_WITH_EGRESS,
-        },
-        {
-          cidrMask: 28,
-          name: "docdb",
-          subnetType: SubnetType.PRIVATE_ISOLATED,
+          name: "private",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
         },
       ],
     });
 
-    // Create an S3 component for hosting
-    this.s3 = new S3(this, "S3", {
-      acm: this.acm,
-      route53: this.route53,
+    // * =====================================
+    // * Route 53
+    // * =====================================
+    // Define the Route 53 hosted zone.
+    const hostedZone = r53.HostedZone.fromLookup(this, "HostedZone", {
+      domainName,
     });
 
-    // Create a security group for DocumentDB and then instantiate DocumentDB
-    const docDbSecurityGroup = new ec2.SecurityGroup(this, "DocDBSecGroup", {
-      vpc: this.vpc,
-      description: "Security group for DocumentDB",
-      allowAllOutbound: true,
+    // * =====================================
+    // * Certificate Manager
+    // * =====================================
+    const certificate = new cmg.Certificate(this, "Certificate", {
+      // Domain name to be secured by the certificate
+      domainName,
+      // Use DNS validation for the certificate.
+      validation: cmg.CertificateValidation.fromDns(hostedZone),
+      // Secure all subdomains under the primary domain
+      subjectAlternativeNames: [`*.${domainName}`],
     });
 
-    this.docDb = new DocumentDB(this, "DocumentDb", {
-      vpc: this.vpc,
-      docDbSecurityGroup,
-      instanceType: InstanceType.of(InstanceClass.R5, InstanceSize.LARGE),
-      instances: 1,
-    });
+    // * =====================================
+    // * DocumentDB
+    // * =====================================
 
-    // Create the ECS instance and pass in ACM, Route53, and DocumentDB
-    this.ecs = new ECS(this, "PayloadECS", {
-      vpc: this.vpc,
-      acm: this.acm,
-      route53: this.route53,
-      docDb: this.docDb,
-    });
-
-    // Allow ECS to access DocumentDB on the MongoDB default port
-    this.docDb.cluster.connections.allowFrom(
-      this.ecs.cluster,
-      ec2.Port.tcp(27017),
+    // Define the security group for the DocumentDB cluster.
+    const ddbSecurityGroup = new ec2.SecurityGroup(
+      this,
+      "DocumentDBSecurityGroup",
+      {
+        vpc: vpc,
+      },
     );
 
-    // Grant ECS task role access to read secrets from Secrets Manager
-    this.ecs.taskDefinition.taskRole.addToPrincipalPolicy(
-      new PolicyStatement({
+    // You can use the `aws-cdk-lib/aws-secretsmanager` to instantiate the
+    // Secrets Manager construct. You can use `generateSecretString` property to
+    // set the condition, such as length, excluding characters and punctuation,
+    // acording to the app and security requirements, for the password that will
+    // be generated. You also set the username that will be used to access the
+    // DocumentDB cluster.
+    const docDbCredentials = new smg.Secret(this, "DocumentDBCredentials", {
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({
+          username: "awsdemo",
+        }),
+        generateStringKey: "password",
+        passwordLength: 16,
+        excludePunctuation: true,
+        excludeCharacters: "/¥'%:;{}",
+      },
+    });
+
+    // You can use `aws-cdk-lib/aws-docdb` module to instantiate the DocumentDB
+    // construct. Specify the ARN of Secrets Manager as `docDbPasswordSecret`,
+    // and define the VPC and subnet where you want to deploy the cluster,
+    // instance type, and number of instances in the properties. You must define
+    // the private subnet on this construct so that DocumentDB isn't accessible
+    // via public endpoint. Here, we create a new DocumentDB cluster which we
+    // attach to the VPC and provide it with the password that we created
+    // previously.
+    const docDbCluster = new ddb.DatabaseCluster(this, "DocDB", {
+      masterUser: {
+        username: docDbCredentials
+          .secretValueFromJson("username")
+          .unsafeUnwrap(),
+        password: cdk.SecretValue.secretsManager(docDbCredentials.secretArn, {
+          jsonField: "password",
+        }),
+      },
+      vpc: vpc,
+      vpcSubnets: vpc.selectSubnets({ subnetGroupName: "private" }),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.MEDIUM,
+      ),
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      // Define the security group for DocumentDB. You must define the security
+      // group to allow incoming traffic from the containerized application's
+      // security group.
+      securityGroup: ddbSecurityGroup,
+    });
+
+    // * =====================================
+    // * Elastic Container Service (ECS)
+    // * =====================================
+
+    // Define the security group for the ECS cluster.
+    const ecsSecurityGroup = new ec2.SecurityGroup(this, "ECSSecurityGroup", {
+      vpc: vpc,
+    });
+
+    // Create an ECS cluster.
+    const ecsCluster = new ecs.Cluster(this, "ECSCluster", { vpc });
+
+    // Create ECS task definition.
+    const ecsTaskDefinition = new ecs.FargateTaskDefinition(
+      this,
+      "TaskDefinition",
+    );
+
+    // Create a new secret for PAYLOAD_SECRET with a random value
+    const payloadSecret = new smg.Secret(this, "PayloadSecret", {
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({}),
+        generateStringKey: "payloadSecret",
+        excludeCharacters: '"@/\\ ',
+      },
+    });
+
+    // Add a container to the task definition.
+    ecsTaskDefinition.addContainer("AppContainer", {
+      image: ecs.ContainerImage.fromAsset(resolve(__dirname, "../../server")),
+      memoryLimitMiB: 512,
+      cpu: 256,
+      secrets: {
+        DOCDB_USERNAME: ecs.Secret.fromSecretsManager(
+          docDbCredentials,
+          "username",
+        ),
+        DOCDB_PASSWORD: ecs.Secret.fromSecretsManager(
+          docDbCredentials,
+          "password",
+        ),
+        PAYLOAD_SECRET: ecs.Secret.fromSecretsManager(
+          payloadSecret,
+          "payloadSecret",
+        ),
+      },
+      portMappings: [
+        {
+          // Expose the container port.
+          containerPort: 3001,
+        },
+      ],
+    });
+
+    // Create an ECS service.
+    const ecsService = new ecs.FargateService(this, "ECSService", {
+      cluster: ecsCluster,
+      taskDefinition: ecsTaskDefinition,
+      securityGroups: [ecsSecurityGroup],
+      assignPublicIp: true,
+      vpcSubnets: vpc.selectSubnets({ subnetGroupName: "public" }),
+    });
+
+    // Application load balancer.
+    const loadBalancer = new elb.ApplicationLoadBalancer(this, "LoadBalancer", {
+      vpc: vpc,
+      internetFacing: true,
+    });
+
+    // Add a listener to the load balancer and targets to listen.
+    const loadBalancerListener = loadBalancer.addListener("Listener", {
+      port: 443,
+      certificates: [certificate],
+    });
+
+    // Add the ECS service as a target to the load balancer listener.
+    loadBalancerListener.addTargets("ECS", {
+      port: 443,
+      targets: [ecsService],
+    });
+
+    // DNS A record pointing to the load balancer
+    new r53.ARecord(this, "BackendPayloadARecord", {
+      zone: hostedZone,
+      target: r53.RecordTarget.fromAlias(
+        new rtg.LoadBalancerTarget(loadBalancer),
+      ),
+      recordName: `${backendSubdomain}.${domainName}`,
+    });
+
+    // Output the DNS name of the load balancer.
+    new cdk.CfnOutput(this, "BackendPayloadURL", {
+      value: loadBalancer.loadBalancerDnsName,
+    });
+
+    // * =====================================
+    // * ECS and DocumentDB interaction setup.
+    // * =====================================
+
+    // Allow incoming traffic from the ECS security group to the DocumentDB
+    // security group.
+    ddbSecurityGroup.addIngressRule(
+      ecsSecurityGroup,
+      ec2.Port.tcp(27017),
+      "Allow MongoDB traffic from ECS",
+    );
+
+    // Grant the ECS task permission to access the DocumentDB credentials.
+    ecsTaskDefinition.taskRole.addToPrincipalPolicy(
+      new iam.PolicyStatement({
         actions: ["secretsmanager:GetSecretValue"],
-        resources: [this.docDb.secret.secretArn],
+        resources: [docDbCredentials.secretArn],
       }),
     );
 
-    // Establish dependency to ensure DocumentDB is created before ECS
-    this.ecs.node.addDependency(this.docDb);
-
-    // Output the DocumentDB endpoint for easy reference
-    new CfnOutput(this, "DocDbEndpoint", {
-      value: this.docDb.cluster.clusterEndpoint.hostname,
-    });
+    // Ensure that the ECS service depends on the DocumentDB cluster. This means
+    // that the ECS service will only be created after the DocumentDB cluster is
+    // created.
+    ecsService.node.addDependency(docDbCluster);
   }
 }
